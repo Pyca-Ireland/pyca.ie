daa63d8530bd114a07dd2f97b7d512c1b948af90
heliosssssssssss <adam.bell.21@portlaoisecbs.com>
2026-01-11 20:07:48 +0000

    [preview] - initial

--- pyca-ireland-landing/src/app/page.tsx
+++ pyca-ireland-landing/src/app/page.tsx
@@ -1,11 +1,29 @@
 import Image from "next/image"
 import Link from "next/link"
 
-export default function main_page() {
+export default function MainPage() {
   return (
     <main className="bg-black min-h-screen">
-      <div className="min-h-screen flex flex-row justify-center">
-        <div className=" border border-red-600 border-dotted min-w-200">test</div>
+      <div className="min-h-screen flex justify-center px-4">
+        <div className="my-8 w-full max-w-xl flex flex-col">
+          
+          <div className="relative w-full aspect-19/7 border border-red-500">
+            <Image
+              src="/pyca_banner.png"
+              alt="Pyca Ireland - Banner"
+              fill /*width*/
+              className="object-contain"
+              sizes="(max-width: 768px) 100vw, 720px"
+              priority
+            />
+          </div>
+
+          <div>
+            Work in progress 
+            TODO: Better font, size, color etc. - Then also implement text
+          </div>
+
+        </div>
       </div>
     </main>
   )
