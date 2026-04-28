28f0a5a71ca0cd0b8d1b010e26b51e081022545a
helios01k <adam.bell.21@portlaoisecbs.com>
2026-04-28 16:45:39 +0100

    attempt 2 prev

--- pyca-ireland-landing/src/app/page.tsx
+++ pyca-ireland-landing/src/app/page.tsx
@@ -118,7 +118,7 @@ export default async function MainPage() {
           </div>
           
           <footer className="flex justify-center mb-4 text-xs">
-            <a href="https://github.com/Pyca-Ireland/lccs-audit"><u>LCCS Course Y"25 Audit</u></a>&nbsp;|&nbsp;educate@pyca.ie
+            <a href="https://github.com/Pyca-Ireland/lccs-audit"><u>LCCS Course Y&apos;25 Audit</u></a>&nbsp;|&nbsp;educate@pyca.ie
           </footer>
 
         </div>
